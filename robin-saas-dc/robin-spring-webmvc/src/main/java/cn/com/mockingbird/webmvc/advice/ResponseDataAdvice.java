8b87d7d6181767adfb333966f31e3d59ce24aad7
zhaopeng <zpen_wy@163.com>
2023-09-19 23:47:36 +0800

    SpringMVC 统一后端接口数据返回格式

---
+++ robin-saas-dc/robin-spring-webmvc/src/main/java/cn/com/mockingbird/webmvc/advice/ResponseDataAdvice.java
@@ -0,0 +1,56 @@
+package cn.com.mockingbird.webmvc.advice;
+
+import cn.com.mockingbird.webmvc.model.ResponseData;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.SneakyThrows;
+import org.springframework.core.MethodParameter;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+/**
+ * 响应数据增强
+ *
+ * @author zhaopeng
+ * @date 2023/9/19 22:50
+ **/
+@RestControllerAdvice
+public class ResponseDataAdvice implements ResponseBodyAdvice<Object> {
+
+    private final ObjectMapper objectMapper;
+
+    public ResponseDataAdvice(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
+
+    /**
+     * 是否支持增强
+     * @param returnType the return type
+     * @param converterType the selected converter type
+     * @return true - 支持；false - 不支持
+     */
+    @SuppressWarnings("all")
+    @Override
+    public boolean supports(MethodParameter returnType, Class<? extends HttpMessageConverter<?>> converterType) {
+        return true;
+    }
+
+    @SuppressWarnings("all")
+    @SneakyThrows
+    @Override
+    public Object beforeBodyWrite(Object body, MethodParameter returnType, MediaType selectedContentType, Class<? extends HttpMessageConverter<?>> selectedConverterType, ServerHttpRequest request, ServerHttpResponse response) {
+        // RestController 方法如果返回 String 类型会直接返回 String 值，于是在这里使用 ResponseData 进行封装
+        if (body instanceof String) {
+            return objectMapper.writeValueAsString(ResponseData.success(body));
+        }
+        // 已经使用 ResponseData 封装过的 body 直接返回
+        if (body instanceof ResponseData) {
+            return body;
+        }
+
+        return ResponseData.success(body);
+    }
+}
---
+++ robin-saas-dc/robin-spring-webmvc/src/main/java/cn/com/mockingbird/webmvc/exception/GlobalExceptionHandler.java
@@ -0,0 +1,33 @@
+package cn.com.mockingbird.webmvc.exception;
+
+import cn.com.mockingbird.webmvc.enums.ResponseCode;
+import cn.com.mockingbird.webmvc.model.ResponseData;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * 全局异常处理器
+ *
+ * @author zhaopeng
+ * @date 2023/9/19 23:34
+ **/
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    /**
+     * 默认的全局异常处理
+     * @param e 异常实例
+     * @return 使用 ResponseData 封装的异常响应实例
+     */
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ResponseData<String> handle(Exception e) {
+        log.error("ExceptionHandler handle exception:", e);
+        return ResponseData.failure(ResponseCode.FAIL.getCode(), e.getMessage());
+    }
+
+}
