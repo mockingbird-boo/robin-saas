333e922190485964f49a928ffa3af67d4137449a
zhaopeng <zpen_wy@163.com>
2023-10-21 03:33:03 +0800

    Redis Starter - 拆分自动配置类

--- robin-saas-dc/robin-redis-spring-boot-starter/src/main/java/cn/com/mockingbird/robin/redis/autoconfigure/EnhancedRedisAutoConfiguration.java
+++ robin-saas-dc/robin-redis-spring-boot-starter/src/main/java/cn/com/mockingbird/robin/redis/autoconfigure/EnhancedRedisAutoConfiguration.java
@@ -1,31 +1,13 @@
 package cn.com.mockingbird.robin.redis.autoconfigure;
 
-import cn.com.mockingbird.robin.common.constant.Standard;
+import cn.com.mockingbird.robin.redis.core.service.RedisLockService;
 import cn.com.mockingbird.robin.redis.core.service.RedisService;
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
-import com.fasterxml.jackson.annotation.PropertyAccessor;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
-import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import org.redisson.api.RedissonClient;
 import org.redisson.spring.starter.RedissonAutoConfiguration;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnSingleCandidate;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.RedisSerializer;
-
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
 /**
  * Redis 自动配置类
@@ -33,45 +15,8 @@ import java.time.format.DateTimeFormatter;
  * @author zhaopeng
  * @date 2023/10/13 22:28
  **/
-@AutoConfiguration(before = RedissonAutoConfiguration.class)
+@AutoConfiguration(after = RedissonAutoConfiguration.class)
 public class EnhancedRedisAutoConfiguration {
-
-    @Bean
-    @ConditionalOnMissingBean(name = "redisTemplate")
-    @ConditionalOnSingleCandidate(RedisConnectionFactory.class)
-    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
-        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
-
-        ObjectMapper objectMapper = new ObjectMapper();
-        // 允许更改底层 VisibilityCheckers 的配置，所有成员字段无需进一步注释即可序列化，而不仅仅是公共字段（默认设置）
-        objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
-        // 指定序列化类必须是非 final 修饰的类
-        objectMapper.activateDefaultTyping(LaissezFaireSubTypeValidator.instance, ObjectMapper.DefaultTyping.NON_FINAL);
-        // LocalDatetime 类型处理
-        JavaTimeModule timeModule = new JavaTimeModule();
-        timeModule.addDeserializer(LocalDateTime.class, new LocalDateTimeDeserializer(DateTimeFormatter.ofPattern(Standard.DateTimePattern.MS_DATETIME)));
-        timeModule.addDeserializer(LocalDate.class, new LocalDateDeserializer(DateTimeFormatter.ofPattern(Standard.DateTimePattern.DATE)));
-        timeModule.addSerializer(LocalDateTime.class, new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(Standard.DateTimePattern.MS_DATETIME)));
-        timeModule.addSerializer(LocalDate.class, new LocalDateSerializer(DateTimeFormatter.ofPattern(Standard.DateTimePattern.DATE)));
-        objectMapper.registerModule(timeModule);
-
-        // 禁用 DATE_AS_TIMESTAMPS
-        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-
-        /*
-         * 采用 GenericJackson2JsonRedisSerializer 序列化方式对于 String、对象、对象数组、JSONObject、JSONArray 的序列化反序列化操作一般都是正常，支持强转，
-         * 而采用 Jackson2JsonRedisSerializer 序列化方式在没有 ObjectMapper 配置时进行对象强转容易发生报错
-         */
-        GenericJackson2JsonRedisSerializer genericJackson2JsonRedisSerializer = new GenericJackson2JsonRedisSerializer(objectMapper);
-        redisTemplate.setKeySerializer(RedisSerializer.string());
-        redisTemplate.setValueSerializer(genericJackson2JsonRedisSerializer);
-        redisTemplate.setHashKeySerializer(RedisSerializer.string());
-        redisTemplate.setHashValueSerializer(genericJackson2JsonRedisSerializer);
-        redisTemplate.setDefaultSerializer(genericJackson2JsonRedisSerializer);
-
-        redisTemplate.setConnectionFactory(connectionFactory);
-        return redisTemplate;
-    }
 
     @Bean
     @ConditionalOnBean(RedisTemplate.class)
@@ -79,5 +24,10 @@ public class EnhancedRedisAutoConfiguration {
         return new RedisService(redisTemplate);
     }
 
+    @Bean
+    @ConditionalOnBean(RedissonClient.class)
+    public RedisLockService redisLockService(RedissonClient redissonClient) {
+        return new RedisLockService(redissonClient);
+    }
 
 }
---
+++ robin-saas-dc/robin-redis-spring-boot-starter/src/main/java/cn/com/mockingbird/robin/redis/autoconfigure/RedisSerializerAutoConfiguration.java
@@ -0,0 +1,86 @@
+package cn.com.mockingbird.robin.redis.autoconfigure;
+
+import cn.com.mockingbird.robin.common.constant.Standard;
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import org.redisson.spring.starter.RedissonAutoConfiguration;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnSingleCandidate;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializer;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * Redis 序列化器自动配置
+ *
+ * @author zhaopeng
+ * @date 2023/10/21 3:20
+ **/
+@AutoConfiguration(before = RedissonAutoConfiguration.class)
+public class RedisSerializerAutoConfiguration {
+
+    @Bean
+    @ConditionalOnMissingBean(name = "redisTemplate")
+    @ConditionalOnSingleCandidate(RedisConnectionFactory.class)
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        // 允许更改底层 VisibilityCheckers 的配置，所有成员字段无需进一步注释即可序列化，而不仅仅是公共字段（默认设置）
+        objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        // 指定序列化类必须是非 final 修饰的类
+        objectMapper.activateDefaultTyping(LaissezFaireSubTypeValidator.instance, ObjectMapper.DefaultTyping.NON_FINAL);
+        // LocalDatetime 类型处理
+        JavaTimeModule timeModule = new JavaTimeModule();
+        timeModule.addDeserializer(LocalDateTime.class, new LocalDateTimeDeserializer(DateTimeFormatter.ofPattern(Standard.DateTimePattern.MS_DATETIME)));
+        timeModule.addDeserializer(LocalDate.class, new LocalDateDeserializer(DateTimeFormatter.ofPattern(Standard.DateTimePattern.DATE)));
+        timeModule.addSerializer(LocalDateTime.class, new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(Standard.DateTimePattern.MS_DATETIME)));
+        timeModule.addSerializer(LocalDate.class, new LocalDateSerializer(DateTimeFormatter.ofPattern(Standard.DateTimePattern.DATE)));
+        objectMapper.registerModule(timeModule);
+
+        // 禁用 DATE_AS_TIMESTAMPS
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
+        /*
+         * 采用 GenericJackson2JsonRedisSerializer 序列化方式对于 String、对象、对象数组、JSONObject、JSONArray 的序列化反序列化操作一般都是正常，支持强转，
+         * 而采用 Jackson2JsonRedisSerializer 序列化方式在没有 ObjectMapper 配置时进行对象强转容易发生报错
+         */
+        GenericJackson2JsonRedisSerializer genericJackson2JsonRedisSerializer = new GenericJackson2JsonRedisSerializer(objectMapper);
+        redisTemplate.setKeySerializer(RedisSerializer.string());
+        redisTemplate.setValueSerializer(genericJackson2JsonRedisSerializer);
+        redisTemplate.setHashKeySerializer(RedisSerializer.string());
+        redisTemplate.setHashValueSerializer(genericJackson2JsonRedisSerializer);
+        redisTemplate.setDefaultSerializer(genericJackson2JsonRedisSerializer);
+
+        redisTemplate.setConnectionFactory(connectionFactory);
+        return redisTemplate;
+    }
+
+    @SuppressWarnings("unused")
+    // @Bean
+    // @ConditionalOnMissingBean(StringRedisTemplate.class)
+    // @ConditionalOnSingleCandidate(RedisConnectionFactory.class)
+    public StringRedisTemplate stringRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        StringRedisTemplate template = new StringRedisTemplate();
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        template.setConnectionFactory(redisConnectionFactory);
+        return template;
+    }
+
+}
