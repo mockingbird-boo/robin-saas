e970dee51d198671a1f3407897b3f3732cbd170c
zhaopeng <zpen_wy@163.com>
2023-10-28 01:41:56 +0800

    Api Security Starter - 初始化 Starter

---
+++ robin-saas-dc/robin-api-security-spring-boot-starter/src/main/java/cn/com/mockingbird/robin/api/security/ApiSecurity.java
@@ -0,0 +1,41 @@
+package cn.com.mockingbird.robin.api.security;
+
+import org.springframework.core.annotation.AliasFor;
+
+import java.lang.annotation.*;
+
+/**
+ * API 安全注解
+ * <p>
+ * 该注解用于标识 API 接口会经过加密、加签来加固接口的安全性。
+ * 安全性包括：防篡改、防重放、响应数据脱敏。
+ *
+ * @author zhaopeng
+ * @date 2023/10/28 1:09
+ **/
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE, ElementType.METHOD})
+@Documented
+public @interface ApiSecurity {
+
+    @AliasFor("sign")
+    boolean value() default true;
+
+    /**
+     * 是否需要签名
+     */
+    @AliasFor("value")
+    boolean sign() default true;
+
+
+    /**
+     * 请求参数是否需要解密
+     */
+    boolean decryptForRequest() default false;
+
+    /**
+     * 响应数据是否需要加密
+     */
+    boolean encryptForResponse() default false;
+
+}
